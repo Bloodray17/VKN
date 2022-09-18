cd4ff30c3f4a97bcc6ef26b21f1a245fee3b851d
Стельмах Юрій <113580224+Bloodray17@users.noreply.github.com>
2022-09-19 01:22:39 +0300

    Add files via upload

---
+++ Лабораторна 3.2.py
@@ -0,0 +1,7 @@
+import sys
+r = int(sys.argv[1])
+h = int(sys.argv[2])
+S = 2*r**2+2*r*h
+V = r**2*h
+print("Площа:",S)
+print("Об'єм:",V)
---
+++ Лабораторна 4.1.py
@@ -0,0 +1,4 @@
+import math
+x=float(input("Введіть x"))
+y=((math.sin(x**2)-math.tan(x-9))/math.log(math.fabs(x**3-math.sin(x))))
+print(y)
