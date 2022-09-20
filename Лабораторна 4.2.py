daa78feb54430bf51b4cab077e8a96c92b40ba26
Стельмах Юрій <113580224+Bloodray17@users.noreply.github.com>
2022-09-20 14:44:08 +0300

    Delete Лабораторна 4.2.py

--- Лабораторна 4.2.py
+++
@@ -1,6 +0,0 @@
-import math
-t=float(input("Введіть значення t:"))
-n=float(input("Введіть значення n:"))
-k=float(input("Введіть значення k:"))
-S=4,17*math.sqrt(t)-math.sin(math.pi*n+math.pi//7)+math.e**(k//t+n)
-print(S)
